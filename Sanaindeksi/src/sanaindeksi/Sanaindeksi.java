aa03b046b7fc64cb54014924f105d1eae3aca505
Sanna Tyrvainen <sanna.tyrvainen@helsinki.fi>
2012-05-18 11:55:48 +0300

    Perjantai 1805: Solmu-olio luotu ja perusjuttuja mietitty

--- Sanaindeksi/src/sanaindeksi/Sanaindeksi.java
+++ Sanaindeksi/src/sanaindeksi/Sanaindeksi.java
@@ -4,6 +4,8 @@
  */
 package sanaindeksi;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author sktyrvai
@@ -14,11 +16,27 @@ public class Sanaindeksi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String ekaSana = "testaus";
+        
+        Solmu j = new Solmu(ekaSana.charAt(0));
+        Solmu s = new Solmu(ekaSana.charAt(1));
+        j.uusiLapsi(s);
+        
         System.out.println("Hello");
         System.out.println("Testaillaan lisää");
+        
+        System.out.println(j.getSolmu());
+        ArrayList<Solmu> l = j.getLapset();
+        System.out.println("lapset:");
+        for(Solmu lapsi: l){
+            System.out.println(l);
+        }
         // tee solmu
         // tee puu yhdellä sanalla
         // lue tekstitiedosto
         // 
+        
     }
+    
+    
 }
